4b59566f952cd1e44f8d2ff2fea5fcda16e3740c
kelvinbiffi <kelvin.biffi@hotmail.com>
2017-02-07 18:38:18 -0200

    Vars console

---
+++ script.js
@@ -0,0 +1,9 @@
+var nomeDaVariavel = 'valor da variável';
+var outraVariavel = 'valor da outra variável';
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+
+console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
