2cc5a4b827116498c5297badef2831fe244c888c
kelvinbiffi <kelvin.biffi@hotmail.com>
2017-02-14 14:08:38 -0200

    Modified script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var nomeDaVariavel = 'valor da variável';
+var nomeDaVariavel = "valor da variável";
 var outraVariavel = 'valor da outra variável';
 var variavelIdade = 22;
 var variavelTenhoCabeloBranco = false;
+var variavelIndefinida;
 
 console.log(nomeDaVariavel);
 console.log(outraVariavel);
 console.log(variavelIdade);
 console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+console.log(variavelNula);
