7a56eb78e921e54164f317cd1e768ccda40e0a57
kelvinbiffi <kelvin.biffi@hotmail.com>
2018-02-07 12:05:36 -0200

    Added logic decision logic codes

---
+++ 7.Decisões lógicas/1.Tabela Verdade/1.E Lógico/script.js
@@ -0,0 +1,8 @@
+var numeroMacas = 4;
+var todasAsMacasSaoVerdes = false;
+
+if (numeroMacas > 1 && todasAsMacasSaoVerdes === true) {
+  console.log("Temos mais de uma maçã e todas as maçãs são verdes.");
+} else {
+  console.log("Ou não temos mais de uma maça ou nem todas são verdes.");
+}
---
+++ 7.Decisões lógicas/1.Tabela Verdade/2.OU Lógico/script.js
@@ -0,0 +1,10 @@
+var numeroCarros = 6;
+var todosCarrosMesmaMarca = false;
+
+if (numeroCarros === 5 && todosCarrosMesmaMarca === true) {
+  console.log("Ou todos os carros são da mesma marca.");
+  console.log("Ou temos cinco carros.");
+  console.log("Ou as duas coisas juntas.");
+} else {
+  console.log("Não temos cinco carros e nem todos são da mesma marca.");
+}
