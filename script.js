91250f1ba83e4ccee2b71bfb6c5bb82475405a8f
Kelvin Biffi <kelvin.biffi@pmweb.local>
2018-05-30 12:27:15 -0300

    Clear main script

--- script.js
+++
@@ -1,9 +0,0 @@
-var primeiraVariavel = "Sou um valor inicial";
-console.log(window.primeiraVariavel);
-
-function novoMetodo() {
-  var varNovoContexto = "Sou uma variável do novo contexto";
-  console.log(varNovoContexto);
-}
-
-novoMetodo();
