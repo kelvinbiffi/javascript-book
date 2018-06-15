4066030f53cf6b68070744dc7be4d0dc2ec51c77
Kelvin Batista Biffi <kelvin.biffi@pmweb.com.br>
2018-06-15 12:50:00 -0300

    Added topic

---
+++ pt/18.Objetos e Funções/6.Primitivos x Objetos/1.Dados primitivos/script.js
@@ -0,0 +1,6 @@
+// dados primitivos
+var nome1 = 'Kayo';
+var nome2 = nome1;
+nome1 = 'Forest';
+
+console.log(nome1, nome2);
---
+++ pt/18.Objetos e Funções/6.Primitivos x Objetos/2.Objetos/script.js
@@ -0,0 +1,8 @@
+// dados primitivos
+var obj1 = {
+  nome: 'Kayo'
+};
+var obj2 = obj1;
+obj2.nome = 'Forest';
+
+console.log(obj1, obj2);
