d872f126de623b9182187187d73cee96cfe34ec3
Kelvin Batista Biffi <kelvin.biffi@pmweb.com.br>
2018-06-18 12:58:44 -0300

    Added a topic

---
+++ pt/18.Objetos e Funções/7.Função de primeira classe/script.js
@@ -0,0 +1,20 @@
+var arrAnoCarros = [1965, 1992, 1997, 1975, 1981];
+
+// Função callback
+function calcularTempoCarro(anoCarro) {
+  var data = new Date();
+  return data.getFullYear() - anoCarro;
+}
+
+function anoDosCarros(arr, fun) {
+  var arrTempoCarros = [];
+  for (var i = 0; i < arrAnoCarros.length; i++) {
+    arrTempoCarros.push(fun(arr[i]));
+  }
+  return arrTempoCarros;
+}
+
+// Passando no segundo parametro uma função
+var arrCarros = anoDosCarros(arrAnoCarros, calcularTempoCarro);
+
+console.log(arrCarros);
