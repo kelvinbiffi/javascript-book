1ac4ee81921e3b2e0d7f40444e28cb25e8a36c4e
kelvinbiffi <kelvinbiffi@hotmail.com>
2018-06-23 07:14:59 -0300

    Added last topoic image

---
+++ pt/15.Bind, Call e Apply/script.js
@@ -0,0 +1,16 @@
+var carro = {
+    ano: 1970,
+    nome: 'Fusca',
+    marca: 'VW',
+    mostrarDados: function (cor) {
+        console.log(this.nome, this.ano, this.marca, cor);
+    }
+}
+
+carro.mostrarDados('Azul');
+
+// Bind
+
+// Call
+
+// Apply
