e9930152f996d9a68dc111adde0c2c1696367f34
Kelvin Batista Biffi <kelvin.biffi@pmweb.com.br>
2018-06-25 09:05:33 -0300

    Added last topic

--- pt/15.Bind, Call e Apply/script.js
+++ pt/15.Bind, Call e Apply/script.js
@@ -6,9 +6,7 @@ var carro = {
     }
 }
 
-carro.mostrarDados('Azul');
-
-// Bind
+carro.mostrarDados('Azul', 1970);
 
 // Call
 carro.mostrarDados.call({
@@ -16,4 +14,19 @@ carro.mostrarDados.call({
     marca: 'Chevrolet'
 }, 'Bordo', 1972);
 
+
+// Bind
+var brasiliaAmarela = carro.mostrarDados.bind({
+    nome: 'Brasilia',
+    marca: 'VW'
+}, 'Bordo');
+
+brasiliaAmarela(1974);
+brasiliaAmarela(1975);
+brasiliaAmarela(1982);
+
 // Apply
+carro.mostrarDados.apply({
+    nome: 'Onix',
+    marca: 'Chevrolet'
+}, ['Cinza', 2016]);
